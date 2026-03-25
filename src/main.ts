5a70e63767c45e896cc2e7cf11851aac67a35e7a
prerna701 <prernaarora457@gmail.com>
2026-03-25 15:27:49 +0530

    performance metrices added

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
