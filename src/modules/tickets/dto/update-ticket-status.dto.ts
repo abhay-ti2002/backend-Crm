f99d3417ee8a61260cd3ac3bb3e1aa326c44cd32
prerna701 <prernaarora457@gmail.com>
2026-03-25 10:35:33 +0530

    api tested

--- src/modules/tickets/dto/update-ticket-status.dto.ts
+++ src/modules/tickets/dto/update-ticket-status.dto.ts
@@ -1,10 +1,5 @@
-import { IsEnum, IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 import { TicketStatus } from '../../../schemas/ticketSchema/ticket.schema';
 
 export class UpdateTicketStatusDto {
-    @ApiProperty({ enum: TicketStatus, example: TicketStatus.RESOLVED })
-    @IsNotEmpty()
-    @IsEnum(TicketStatus)
     status: TicketStatus;
 }
