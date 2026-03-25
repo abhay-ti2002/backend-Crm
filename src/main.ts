8b1b01b4443e15610c91cc8e44ccef98c1066ff0
abhay-ti2002 <abhi.tiwari2027@gmail.com>
2026-03-25 17:44:19 +0530

    integration of cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
